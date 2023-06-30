99023aa96c2a295144ef2bd82983de06aa139f95
BBBOYGIRL <41622623+BBBOYGIRL@users.noreply.github.com>
2023-06-30 14:38:13 +0800

    Update run_net.py

--- tools/run_net.py
+++ tools/run_net.py
@@ -6,7 +6,7 @@ from timesformer.utils.parser import load_config, parse_args
 
 from tools.test_net import test
 from tools.train_net import train
-
+dsfsdff
 
 def get_func(cfg):
     train_func = train
